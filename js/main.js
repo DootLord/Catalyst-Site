4b60c89f400c6328d4c58d79fc1c0bbe94e39235
DootLord <shaunoman840@gmail.com>
2025-03-30 20:21:27 +0100

    main js cleanup

--- js/main.js
+++ js/main.js
@@ -5,66 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
         once: true,
     });
 
-    // Initialize tsparticles
-    tsParticles.load("particles-js", {
-        particles: {
-            number: {
-                value: 100,
-                density: {
-                    enable: true,
-                    value_area: 800,
-                },
-            },
-            color: {
-                value: ["#ff00ff", "#00ffff"],
-            },
-            shape: {
-                type: "circle",
-            },
-            opacity: {
-                value: 0.5,
-                random: true,
-            },
-            size: {
-                value: 5,
-                random: true,
-            },
-            move: {
-                enable: true,
-                speed: 2,
-                direction: "none",
-                random: false,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-            },
-        },
-        interactivity: {
-            detect_on: "canvas",
-            events: {
-                onhover: {
-                    enable: true,
-                    mode: "repulse",
-                },
-                onclick: {
-                    enable: true,
-                    mode: "push",
-                },
-                resize: true,
-            },
-            modes: {
-                repulse: {
-                    distance: 100,
-                    duration: 0.4,
-                },
-                push: {
-                    particles_nb: 4,
-                },
-            },
-        },
-        retina_detect: true,
-    });
-
     // Back to Top Button
     const backToTop = document.querySelector('.back-to-top');
     window.addEventListener('scroll', () => {
